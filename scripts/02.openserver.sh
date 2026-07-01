cd8103f7d32c4fdb639060cb9aae484344c6fecd
Code Devper <codedevper@gmail.com>
2026-07-01 17:53:29 +0700

    Remove Composer installation from openserver script
    Removed Composer installation steps from the script.

--- scripts/02.openserver.sh
+++ scripts/02.openserver.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 
 sudo -u $USER -H bash <<EOF
-cd $HOME
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'c8b085408188070d5f52bcfe4ecfbee5f727afa458b2573b8eaaf77b3419b0bf2768dc67c86944da1544f06fa544fd47') { echo 'Installer verified'.PHP_EOL; } else { echo 'Installer corrupt'.PHP_EOL; unlink('composer-setup.php'); exit(1); }"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-
-sudo mv composer.phar /usr/local/bin/composer
-
-which composer
-
 git clone https://github.com/codedevper/laravel-exp.git "$HOME/.openserver"
 
 cd $HOME/.openserver
