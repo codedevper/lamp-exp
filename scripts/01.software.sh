bad615663efe4bc122402821ca229eb096fb05f3
Code Devper <codedevper@gmail.com>
2026-07-01 17:54:48 +0700

    Refactor script to source Docker setup correctly
    Reorganize sourcing of scripts and remove redundant Docker setup commands.

--- scripts/01.software.sh
+++ scripts/01.software.sh
@@ -7,12 +7,8 @@ source "$BASH_DIR/scripts/lib/php.sh"
 sudo a2enmod rewrite proxy_fcgi
 sudo systemctl restart apache2
 
+source "$BASH_DIR/scripts/lib/composer.sh"
+
 source "$BASH_DIR/scripts/lib/nodejs.sh"
 
 source "$BASH_DIR/scripts/lib/docker.sh"
-
-sudo usermod -aG docker $USER
-
-sudo mkdir -p $HOME/.docker
-sudo chown $USER:$USER $HOME/.docker -R
-sudo chmod g+rwx "$HOME/.docker" -R
